f4f7bd70c82f16639c827d5bd8de7baab325461b
javirfo775 <133286155+javirfo775@users.noreply.github.com>
2023-06-18 00:58:40 +0200

    Add files via upload

---
+++ tfg/validaciones.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var form = document.querySelector('form');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var nombre = document.getElementById('nombre').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirm-password').value;
+
+    if (nombre === '' || email === '' || password === '' || confirmPassword === '') {
+      alert('Por favor, complete todos los campos.');
+      return;
+    }
+
+    if (/[\d]/.test(nombre)) {
+      alert('El campo nombre no debe contener números.');
+      return;
+    }
+
+    if (!email.includes('@')) {
+      alert('El campo email debe contener el carácter "@"');
+      return;
+    }
+
+    if (password.length < 8) {
+      alert('La contraseña debe tener al menos 8 caracteres.');
+      return;
+    }
+
+    if (!/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/.test(password)) {
+      alert('La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial.');
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      alert('Las contraseñas no coinciden.');
+      return;
+    }
+
+    // Si todas las validaciones pasan, puedes realizar alguna acción adicional aquí, como enviar el formulario.
+    // alert('Registro exitoso!');
+    // form.reset();
+  });
+});

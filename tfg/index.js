e6615ef87831205bb68e5f023a689998f6c34566
javirfo775 <133286155+javirfo775@users.noreply.github.com>
2023-06-18 19:21:16 +0200

    Update index.js

--- tfg/index.js
+++ tfg/index.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
+  var userEmail=localStorage.getItem("useremail");
   var downloadLink = document.getElementById('download-link');
   var filename = 'prueba.txt'; // Nombre del archivo a descargar
-
+  document.getElementById('user-info').textContent=userEmail;
+  
   downloadLink.addEventListener('click', function(event) {
     event.preventDefault();
     downloadFile(filename);
